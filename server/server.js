4fa980f1a7e57095c7bd600d044c562080f648b0
Fadilulahi Opeyemi Musediq <musediqopeyemi@gmail.com>
2026-06-24 14:34:04 -0400

    server connected successfully

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+import express from 'express';
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/db.js';
+
+connectDB()
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('Server is running'))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> console.log(`Server is running on PORT ${PORT}`))
